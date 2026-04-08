715a6d294bd48a197178a315874c66e13c5dc2f2
Amirah Rizkita Setiadji <amirah.rizkita@ui.ac.id>
2026-04-08 23:25:10 +0700

    Add booking flow

--- pitpet_clinic/urls.py
+++ pitpet_clinic/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from booking import views as booking_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('', lambda request: redirect('login')),
     path('user-profile/', include('user_profile.urls')),
     path('groomer/', include('groomer.urls')),
-    path('groomer-jobs/', include('groomer_jobs.urls')),
+    path('booking/', include(('booking.urls', 'booking'), namespace='booking')),
+    path('api/slots/', booking_views.api_slots, name='api_slots'),
 ]
