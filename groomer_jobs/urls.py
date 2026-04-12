e90a9f805fc64d9b3395da9f8c4c8cd5a954bb2c
Salsabila Caturizky Farindarmawan <salsabila.caturizky@ui.ac.id>
2026-04-13 01:38:30 +0700

    fitur detail pekerjaan & ubah status

--- groomer_jobs/urls.py
+++ groomer_jobs/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from . import views
+from .views import *
+
+# urlpatterns = [
+#     path('', views.daily_job_list, name='daily_job_list'),
+# ]
+
+app_name = "groomer_jobs"
 
 urlpatterns = [
-    path('', views.daily_job_list, name='daily_job_list'),
+    path("dashboard/", daily_job_list, name="daily_job_list"),
+    path("bookings/<int:booking_id>/", job_detail, name="job_detail"),
+    path("bookings/<int:booking_id>/status/", update_booking_status, name="update_booking_status"),
 ]
