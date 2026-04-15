5c7830db8b00f6626cdaef33778f3ff0fda98675
abbytamp <abbyst14@gmail.com>
2026-04-15 09:59:04 +0700

    save progress before switching branch

--- user_profile/views.py
+++ user_profile/views.py
@@ -6,7 +6,7 @@ from pet.models import Pet
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib import messages
-from django.db import IntegrityError  # 🔥 TAMBAHAN
+from django.db import IntegrityError  
 
 
 @login_required
@@ -85,7 +85,6 @@ def edit_profile_view(request):
     return render(request, "user_profile/edit_profile.html")
 
 
-# 🔥 FIX UTAMA DI SINI
 @login_required
 def add_pet(request):
     if request.method == 'POST':
